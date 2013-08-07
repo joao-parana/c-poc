f845c8374dc3528ec7a69d04954b67a5e9b6dd12
João Antonio Ferreira <joao.parana@gmail.com>
2013-08-07 18:27:50 -0300

    acertando alguns erros

--- MyApp/www/js/index.js
+++ MyApp/www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,7 +15,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+		// alert('onDeviceReady called');
+        app.receivedEvent('deviceready');		
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -45,5 +28,61 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+		app.doIt();
+    },
+	doIt: function() {
+		// alert('doIt called');
+		// setTimeout(getPictureFromCamera, 10);
     }
 };
+
+function getPictureFromCamera()  {
+	// alert('getPictureFromCamera called');
+	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+		destinationType: Camera.DestinationType.DATA_URL
+	});
+}
+// <img id="myImage_1" style="width: 100px;"></img>
+// <img id="myImage_2" style="width: 100px;"></img>
+// ...
+var myCounterForImages = 1;
+function addImageElementToDOM(elem) {
+  // alert('addImageElementToDOM called');
+  // create a new img element
+  // and give it some content
+  var textNodeContent = ' ' + myCounterForImages + ' ';
+  var imgElemId = "myImage_" + myCounterForImages;
+  myCounterForImages++;
+  var newDiv = document.createElement("div");
+  var newContent = document.createTextNode(textNodeContent);
+  newDiv.appendChild(newContent);
+  if (elem) {
+    newDiv.appendChild(elem);
+  }
+  var newImg = document.createElement("img");
+  newImg.id = imgElemId;
+  newDiv.appendChild(newImg);
+  // add the newly created element and its content into the DOM
+  var myDivConteinerFirstChild = document.getElementById("all-images-header");
+  // alert('7');
+  // var image = document.getElementById(imgElemId);
+  var parentDiv = myDivConteinerFirstChild.parentNode;
+  parentDiv.insertBefore(newDiv, myDivConteinerFirstChild);
+  // alert('8');
+  return imgElemId;
+}
+
+function onSuccess(imageData) {
+	// alert('onSuccess called');
+	var editControl; // one optional checkbox to select image
+	var imgElemId = addImageElementToDOM();
+	// alert('imgElemId = ' + imgElemId + ", imageData.length=" + imageData.length);
+    var image = document.getElementById(imgElemId);
+    image.src = "data:image/jpeg;base64," + imageData;
+	image.width = 100;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
